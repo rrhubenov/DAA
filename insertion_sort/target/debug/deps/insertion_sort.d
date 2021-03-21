2d5b61a3bcd505d9f7b26666b2d26c04262bcec8
Radoslav Hubenov <rrhubenov@gmail.com>
2021-03-21 11:37:52 +0200

    second try to remove target

--- insertion_sort/target/debug/deps/insertion_sort.d
+++
@@ -1,5 +0,0 @@
-/Users/rado/Uni/DAA/insertion_sort/target/debug/deps/insertion_sort: src/main.rs
-
-/Users/rado/Uni/DAA/insertion_sort/target/debug/deps/insertion_sort.d: src/main.rs
-
-src/main.rs:
--- insertion_sort/target/debug/insertion_sort.d
+++
@@ -1,1 +0,0 @@
-/Users/rado/Uni/DAA/insertion_sort/target/debug/insertion_sort: /Users/rado/Uni/DAA/insertion_sort/src/main.rs
