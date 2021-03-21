d7d1d93f19f4db8f8f3be722ce5f209af5357a75
Radoslav Hubenov <rrhubenov@gmail.com>
2021-03-21 11:41:35 +0200

    remove target for good

--- insertion_sort/target/rls/debug/deps/insertion_sort-34bab2a5b6cec286.d
+++
@@ -1,5 +0,0 @@
-/Users/rado/Uni/DAA/insertion_sort/target/rls/debug/deps/insertion_sort-34bab2a5b6cec286.rmeta: src/main.rs
-
-/Users/rado/Uni/DAA/insertion_sort/target/rls/debug/deps/insertion_sort-34bab2a5b6cec286.d: src/main.rs
-
-src/main.rs:
--- insertion_sort/target/rls/debug/deps/insertion_sort-40e03a834dd61530.d
+++
@@ -1,5 +0,0 @@
-/Users/rado/Uni/DAA/insertion_sort/target/rls/debug/deps/insertion_sort-40e03a834dd61530.rmeta: src/main.rs
-
-/Users/rado/Uni/DAA/insertion_sort/target/rls/debug/deps/insertion_sort-40e03a834dd61530.d: src/main.rs
-
-src/main.rs:
